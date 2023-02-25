e34072241fc5a4c8ca3ed00ef3c3fa2e2384849d
uttamsaha <uttamsaha982@live.com>
2023-02-25 12:32:46 +0600

    updated

--- contracts/ExampleExceptionRequire.sol
+++ contracts/ExampleExceptionRequire.sol
@@ -1,23 +1,25 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.0;
+pragma solidity 0.6.12;
 
-contract ExampleExceptionRequire {
-    mapping(address => uint) balanceReceived;
+contract ExceptionExample {
+
+    mapping(address => uint) public balanceReceived;
 
     function receiveMoney() public payable {
         balanceReceived[msg.sender] += msg.value;
-
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        // if(amout <= balanceReceived[msg.sender]){
+        //  if(_amount <= balanceReceived[msg.sender]) {
         //     balanceReceived[msg.sender] -= _amount;
-        //     _to.transfer(_amount)
+        //     _to.transfer(_amount);
         // }
-        require(amout <= balanceReceived[msg.sender], "Not enough funds")
+
+        // require diye if er same kaj tai kora hoise but ekhane ektu validation kora hoise. first e smart contract e 100 gwei send korlam then 101 gwei witdraw korbo tahole transaction faild hobe error dibe.. if die korle transaction hobe but withdraw hobe na..
+        require(_amount <= balanceReceived[msg.sender], "Not Enough Funds, aborting");
+
         balanceReceived[msg.sender] -= _amount;
-        _to.transfer(_amount)
-        
+        _to.transfer(_amount);
     }
 }
